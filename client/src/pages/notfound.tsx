1bdec33f55c4fcaff0ae459a713064195753519f
viiccwen <viiccwen@gmail.com>
2024-12-28 01:55:59 +0800

    feat: notfound page

--- client/src/pages/notfound.tsx
+++ client/src/pages/notfound.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Undo2 } from "lucide-react";
+
 export default function NotFound() {
     return (
-      <>
-        <h1 className="text-3xl font-bold underline">404 Not Found!</h1>
-      </>
+      <div className="flex flex-col w-full justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold">404 Not Found</h1>
+        <Button LinkTo="/" variant="default" className="mt-5 text-xl">Go Back <Undo2 className="w-3" /></Button>
+      </div>
     );
   }
   
