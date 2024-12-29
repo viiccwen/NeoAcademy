c1c617fba3251331c33dbc5b85867f8e45973158
viiccwen <viiccwen@gmail.com>
2024-12-29 14:57:02 +0800

    feat: user-actions
    1. user API actions (get user, delete user)
    2. change oauth URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import NotFound from "./pages/notfound";
 import Login from "./pages/login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} /> {/* 404 頁面 */}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
