9d0a5be8c66d5a286f73e623c5363a4d97983f7e
viiccwen <viiccwen@gmail.com>
2025-01-03 01:52:30 +0800

    feat: change theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,24 @@ import Login from "./pages/login";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Quiz from "./pages/quiz";
 import Create from "./pages/create";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/quiz/:quizId/:index" element={<Quiz />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/quiz/:quizId/:index" element={<Quiz />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
