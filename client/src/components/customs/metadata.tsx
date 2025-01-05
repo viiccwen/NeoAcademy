41b7a8c00235e1148b2a5ed422d18772c258a6b6
viiccwen <viiccwen@gmail.com>
2025-01-05 12:02:12 +0800

    chore: change metadata

--- client/src/components/customs/metadata.tsx
+++ client/src/components/customs/metadata.tsx
@@ -8,8 +8,13 @@ interface MetadataProps {
 export const Metadata = (props: MetadataProps) => {
   return (
     <Helmet>
-      <title>{`${props.title} - NeoAcademy AI 新智學習平台`}</title>
-      <meta name="description" content={props.description || "Personalized Learning, Powered by AI"} />
+      <title>{`${
+        props.title ? props.title + " - " : ""
+      }NeoAcademy AI 新智學習平台`}</title>
+      <meta
+        name="description"
+        content={props.description || "Personalized Learning, Powered by AI"}
+      />
     </Helmet>
   );
 };
